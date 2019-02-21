1218cc8b94c94813ee166ad4365ca5b2805ffb30
Aref <rf.gholmi@yahoo.com>
2019-02-22 01:01:57 +0330

    add auth server

---
+++ auth-server/src/main/java/com/rfgholami/samples/microservices/authserver/AuthServer.java
@@ -0,0 +1,40 @@
+package com.rfgholami.samples.microservices.authserver;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.net.ssl.HttpsURLConnection;
+import java.security.Principal;
+
+
+@RestController
+@EnableResourceServer
+@EnableAuthorizationServer
+@EnableDiscoveryClient
+@SpringBootApplication
+public class AuthServer {
+
+    private static final Logger LOG = LoggerFactory.getLogger(AuthServer.class);
+
+    static {
+        // for localhost testing only
+//        LOG.warn("Will now disable hostname check in SSL, only to be used during development");
+//        HttpsURLConnection.setDefaultHostnameVerifier((hostname, sslSession) -> true);
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthServer.class, args);
+    }
+
+    @RequestMapping("/user")
+    public Principal user(Principal user) {
+        return user;
+    }
+}
--- gateway-application/src/main/java/com/rfgholami/samples/microservices/gatewayapplication/GatewayApplication.java
+++ gateway-application/src/main/java/com/rfgholami/samples/microservices/gatewayapplication/GatewayApplication.java
@@ -9,8 +9,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.web.bind.annotation.RestController;
 
+@EnableResourceServer
 @SpringBootApplication
 @EnableZuulProxy
 @RestController
